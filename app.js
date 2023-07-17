74d2fe8e615fba69601b5b639e33dd906f342056
Sagar Bakshi <sagar.bakshi@terumoamericas.com>
2023-07-18 01:23:14 +0530

    added authentication

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import path from "path";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const app = express();
 
@@ -19,6 +21,7 @@ try {
 const UserSchema = new mongoose.Schema({
   name: String,
   email: String,
+  password: String,
 });
 
 const User = mongoose.model("User", UserSchema);
@@ -30,9 +33,12 @@ app.use(express.static(path.join(path.resolve(), "public")));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-const isAuthenticated = (req, res, next) => {
+const isAuthenticated = async (req, res, next) => {
   const { token } = req.cookies;
+
   if (token) {
+    const decoded = jwt.verify(token, "iamasecret");
+    req.user = await User.findById(decoded._id);
     next();
   } else {
     res.render("login");
@@ -40,8 +46,9 @@ const isAuthenticated = (req, res, next) => {
 };
 
 // Routes below
+//root url
 app.get("/", isAuthenticated, (req, res) => {
-  res.render("logout");
+  res.render("logout", { name: req.user.name });
 });
 
 app.get("/get", async (req, res) => {
@@ -50,28 +57,77 @@ app.get("/get", async (req, res) => {
   res.json(users);
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+// Login URL
 app.post("/login", async (req, res) => {
-  const { name, email } = req.body;
-  console.log(name, email);
-  const user = await User.create({
-    name,
-    email,
-  });
+  const { name, email, password } = req.body;
 
-  res.cookie("token", user._id, {
+  const user = await User.findOne({ email: email });
+
+  if (!user) return res.redirect("/register");
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch)
+    return res.render("login", { email, message: "incorrect password" });
+
+  const token = jwt.sign({ _id: user._id }, "iamasecret");
+  console.log(token);
+
+  res.cookie("token", token, {
     httpOnly: true,
     expires: new Date(Date.now() + 60 * 1000),
   });
 
   res.redirect("/");
+
+  // const user = await User.create({
+  //   name,
+  //   email,
+  // });
 });
 
+//Logout
 app.get("/logout", (req, res) => {
   res.cookie("token", null, {
     httpOnly: true,
     expires: new Date(Date.now()),
   });
   res.send("cookie deleted");
+});
+
+//Register
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  let user = await User.findOne({ email });
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  if (user) {
+    return res.redirect("/login");
+  }
+  user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  const token = jwt.sign({ _id: user._id }, "iamasecret");
+  console.log(token);
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 60 * 1000),
+  });
+
+  return res.redirect("/");
 });
 
 // Server listening function
